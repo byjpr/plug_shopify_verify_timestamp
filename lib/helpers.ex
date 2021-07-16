e914d59d4f26caad33ac4b9d869a80bcf9c05d4c
Jordan Parker <jgit@fmail.co.uk>
2021-07-17 00:32:59 +0100

    add specs

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -4,6 +4,7 @@ defmodule PlugShopifyVerifyTimestamp.Helper do
   """
   import Plug.Conn
 
+  @spec halt_on_error?(Plug.Conn.t(), boolean, atom) :: Plug.Conn.t()
   def halt_on_error?(conn, true, pass_signal) do
     case conn.private[pass_signal] do
       false ->
@@ -18,6 +19,10 @@ defmodule PlugShopifyVerifyTimestamp.Helper do
     conn
   end
 
+  @spec get_param_from_url(
+          atom | %{:params => nil | maybe_improper_list | map, optional(any) => any},
+          any
+        ) :: any
   def get_param_from_url(conn, %{param: key}), do: get_param_from_url(conn, key)
 
   def get_param_from_url(conn, key) when is_atom(key),
@@ -25,5 +30,6 @@ defmodule PlugShopifyVerifyTimestamp.Helper do
 
   def get_param_from_url(conn, key), do: conn.params[key]
 
+  @spec get_param_from_header(Plug.Conn.t(), binary) :: [binary]
   def get_param_from_header(conn, key), do: get_req_header(conn, key)
 end
