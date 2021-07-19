db893d4edb2718c4597ac83b7f20782607199295
Jordan Parker <jgit@fmail.co.uk>
2021-07-19 22:51:31 +0000

    ✅ Working extracted tests

--- test/plug_shopify_verify_timestamp_test.exs
+++ test/plug_shopify_verify_timestamp_test.exs
@@ -1,107 +1,105 @@
 defmodule PlugShopifyVerifyTimestampTest do
-  use ExUnit.Case
-  doctest PlugShopifyVerifyTimestamp
-
   @moduledoc """
   Test the plug that verifies time difference supplied in URL Parameters by Shopify.
   """
 
-  use Enron.ConnCase
+  use ExUnit.Case
   use Timex
-  alias EnronWeb.{Plug, Router}
+  use Plug.Test
 
-  setup _config do
-    conn = bypass_through(build_conn(), Router, :browser)
+  doctest PlugShopifyVerifyTimestamp
 
-    {:ok, %{conn: conn}}
+  defp parse(conn, opts \\ []) do
+    opts = Keyword.put_new(opts, :parsers, [Plug.Parsers.URLENCODED, Plug.Parsers.MULTIPART])
+    Plug.Parsers.call(conn, Plug.Parsers.init(opts))
   end
 
-  test "halts connections without parameter", %{conn: conn} do
+  test "halts connections without parameter" do
     config = [max_delta: 5, halt_on_error: true]
 
     conn =
-      conn
+      conn(:get, "/new")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     assert conn.halted
   end
 
-  test "halts connections with time delay", %{conn: conn} do
+  test "halts connections with time delay" do
     config = [max_delta: 5, halt_on_error: true]
     datetime = Timex.now() |> Timex.shift(seconds: -6) |> Timex.to_unix()
 
     conn =
-      conn
+      conn(:get, "/new?test_param=param&timestamp=#{datetime}")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new?test_param=param&timestamp=#{datetime}")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     assert conn.halted
   end
 
-  test "1 second delay with a max delta of 5 should not be halted", %{conn: conn} do
+  test "1 second delay with a max delta of 5 should not be halted" do
     config = [max_delta: 5, halt_on_error: true]
     datetime = Timex.now() |> Timex.shift(seconds: -1) |> Timex.to_unix()
 
     conn =
-      conn
+      conn(:get, "/new?test_param=param&timestamp=#{datetime}")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new?test_param=param&timestamp=#{datetime}")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     refute conn.halted
   end
 
-  test "2 second delay with a max delta of 5 should not be halted", %{conn: conn} do
+  test "2 second delay with a max delta of 5 should not be halted" do
     config = [max_delta: 5, halt_on_error: true]
     datetime = Timex.now() |> Timex.shift(seconds: -2) |> Timex.to_unix()
 
     conn =
-      conn
+      conn(:get, "/new?test_param=param&timestamp=#{datetime}")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new?test_param=param&timestamp=#{datetime}")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     refute conn.halted
   end
 
-  test "3 second delay with a max delta of 5 should not be halted", %{conn: conn} do
+  test "3 second delay with a max delta of 5 should not be halted" do
     config = [max_delta: 5, halt_on_error: true]
     datetime = Timex.now() |> Timex.shift(seconds: -3) |> Timex.to_unix()
 
     conn =
-      conn
+      conn(:get, "/new?test_param=param&timestamp=#{datetime}")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new?test_param=param&timestamp=#{datetime}")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     refute conn.halted
   end
 
-  test "4 second delay with a max delta of 5 should not be halted", %{conn: conn} do
+  test "4 second delay with a max delta of 5 should not be halted" do
     config = [max_delta: 5, halt_on_error: true]
     datetime = Timex.now() |> Timex.shift(seconds: -4) |> Timex.to_unix()
 
     conn =
-      conn
+      conn(:get, "/new?test_param=param&timestamp=#{datetime}")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new?test_param=param&timestamp=#{datetime}")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     refute conn.halted
   end
 
-  test "5 second delay with a max delta of 5 should be halted", %{conn: conn} do
+  test "5 second delay with a max delta of 5 should be halted" do
     config = [max_delta: 5, halt_on_error: true]
     datetime = Timex.now() |> Timex.shift(seconds: -5) |> Timex.to_unix()
 
     conn =
-      conn
+      conn(:get, "/new?test_param=param&timestamp=#{datetime}")
+      |> parse()
       |> put_private(:shop_origin_type, :url)
-      |> get("/new?test_param=param&timestamp=#{datetime}")
-      |> Enron.URLAuthTime.call(config)
+      |> PlugShopifyVerifyTimestamp.call(config)
 
     assert conn.halted
   end
